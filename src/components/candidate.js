594a63eb4a93b7967ad10ce32c3c6426689fa4a0
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-08-01 17:11:32 -0400

    Proptypes for topic and candidate

--- src/components/candidate.js
+++ src/components/candidate.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Candidate = ({ candidate }) => <li>{candidate.name}</li>;
+
+Candidate.propTypes = {
+  candidate: PropTypes.object,
+};
 
 export default Candidate;
--- src/components/topic.js
+++ src/components/topic.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Topic = ({ topic }) => <li>{topic.name}</li>;
+
+Topic.propTypes = {
+  topic: PropTypes.object,
+};
 
 export default Topic;
