cb00b2edd48aafb08702e5dcc97e6fe1969358c5
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-13 14:32:51 -0400

    Output topics and description as lists

--- src/components/plan.js
+++ src/components/plan.js
@@ -2,19 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import topics from '../generatedData/topics.json';
+
 const StyledPlan = styled.div`
   display: grid;
-  grid-template-columns: auto 1fr;
+  grid-template-columns: minmax(150px, 20%) 1fr;
+`;
+
+const TopicsList = styled.ul`
+  list-style: none;
+  margin: 0 0.25rem;
+  padding: 0;
 `;
 
 const Plan = ({ candidate, plans }) => (
   <StyledPlan>
     <div>{candidate.name}</div>
-    <div>
-      {plans.map(plan => (
-        <p>{plan.plan}</p>
-      ))}
-    </div>
+    <TopicsList>
+      {plans.map(plan => {
+        const topic = topics.find(t => t.id === plan.topic);
+        return (
+          <li>
+            <h3>{topic.name}</h3>
+            {plan.plan}
+          </li>
+        );
+      })}
+    </TopicsList>
   </StyledPlan>
 );
 
