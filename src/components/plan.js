722f8c4deda618c74566dc3314576e32502ece09
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-10-21 12:51:27 -0400

    Sort plans before displaying them

--- src/components/plan.js
+++ src/components/plan.js
@@ -41,31 +41,40 @@ const Plan = ({ candidate, plans }) => (
     <div>{candidate.name}</div>
     {plans.length ? (
       <TopicsList>
-        {plans.map(plan =>
-          topics
-            .filter(t => t.id === plan.topic)
-            .map(topic => (
-              <Topic key={`${candidate.id}-${topic.id}`}>
-                <TopicHeading>{topic.name}</TopicHeading>
-                <TopicDescription>{plan.plan}</TopicDescription>
-                {plan.link && plan.link !== '' && (
-                  <ReadMore
-                    href={plan.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Read More
-                  </ReadMore>
-                )}
-              </Topic>
-            ))
-        )}
+        {plans
+          .sort(
+            (a, b) =>
+              topics.find(t => t.id === a.topic).order >
+              topics.find(t => t.id === b.topic).order
+          )
+          .map(plan =>
+            topics
+              .filter(t => t.id === plan.topic)
+              .map(topic => (
+                <Topic key={`${candidate.id}-${topic.id}`}>
+                  <TopicHeading>{topic.name}</TopicHeading>
+                  {plan.plan.split(/\n/).map(s => (
+                    <TopicDescription>{s}</TopicDescription>
+                  ))}
+                  {plan.link && plan.link !== '' && (
+                    <ReadMore
+                      href={plan.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Read More
+                    </ReadMore>
+                  )}
+                </Topic>
+              ))
+          )}
       </TopicsList>
     ) : (
-      <p>Sorry, so plans match these topics for this candidate.</p>
+      <p>Sorry, no plans match these topics for this candidate.</p>
     )}
   </StyledPlan>
 );
+
 export default Plan;
 
 Plan.propTypes = {
