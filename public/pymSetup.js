f5420591a5383774a5a3c3c1478bc86b97fd8541
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-11-15 14:05:02 -0500

    Prep for embed with pym

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+  const pymParent = new pym.Parent(
+    '2020-tracker',
+    'https://2020-presidential-tax-tracker.netlify.com/index.html',
+    {}
+  );
+});
