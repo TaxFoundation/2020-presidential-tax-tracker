4c6a1ba5c75cbe2a2261a933a2765708967e077e
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-02-05 10:44:00 -0500

    Split out the selection of running and dropped candidates

--- src/components/selection/candidatesSelect.js
+++ src/components/selection/candidatesSelect.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import CandidateSelect from './candidateSelect';
 import Button from '../ui/button';
 import { StyledUnorderedList, StyledListItem } from '../ui/list';
 import { alphabeticalSort } from '../../utilities';
 
+const SectionHeading = styled.h3`
+  border-bottom: 1px solid ${props => props.theme.borderColor};
+  color: ${props => props.theme.color};
+  margin: 0 0 1rem;
+  text-align: center;
+`;
+
 const CandidatesSelect = ({ candidates }) => (
-  <StyledUnorderedList>
-    {candidates
-      .sort((a, b) => alphabeticalSort(a.lastName, b.lastName))
-      .map(candidate => (
-        <StyledListItem key={`${candidate.id}-select`}>
-          <CandidateSelect candidate={candidate} />
-        </StyledListItem>
-      ))}
-    <StyledListItem>
-      <Button id="toggleCandidates">Select / Deselect All</Button>
-    </StyledListItem>
-  </StyledUnorderedList>
+  <>
+    <SectionHeading>Running</SectionHeading>
+    <StyledUnorderedList>
+      {candidates
+        .filter(candidate => candidate.running)
+        .sort((a, b) => alphabeticalSort(a.lastName, b.lastName))
+        .map(candidate => (
+          <StyledListItem key={`${candidate.id}-select`}>
+            <CandidateSelect candidate={candidate} />
+          </StyledListItem>
+        ))}
+      <StyledListItem>
+        <Button id="toggleRunningCandidates">Select / Deselect All</Button>
+      </StyledListItem>
+    </StyledUnorderedList>
+    <SectionHeading>Dropped Out</SectionHeading>
+    <StyledUnorderedList>
+      {candidates
+        .filter(candidate => !candidate.running)
+        .sort((a, b) => alphabeticalSort(a.lastName, b.lastName))
+        .map(candidate => (
+          <StyledListItem key={`${candidate.id}-select`}>
+            <CandidateSelect candidate={candidate} />
+          </StyledListItem>
+        ))}
+      <StyledListItem>
+        <Button id="toggleDroppedCandidates">Select / Deselect All</Button>
+      </StyledListItem>
+    </StyledUnorderedList>
+  </>
 );
 
 CandidatesSelect.propTypes = {
